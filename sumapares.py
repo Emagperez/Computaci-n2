68d12772ef5eb7197c204de8b046c4de5b0d03b6
emagperez <eg.perez@alumno.um.edu.ar>
2022-04-03 19:43:08 -0300

    sumapares sin terminar

---
+++ sumapares.py
@@ -0,0 +1,15 @@
+import os, argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", type=int, help="Numero de procesos que se quieren generar", required=True)
+    parser.add_argument("-v",help="-v para habiltiar modo verboso",action="stroe true",default=False)
+    args = parser.parse_args
+
+def suma()
+    
+
+
+if __name__ == "__main__:"
+    main()
