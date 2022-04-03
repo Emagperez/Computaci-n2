041af448a53c9b3fb0ac0f213e9897c1df4cb66e
emagperez <eg.perez@alumno.um.edu.ar>
2022-04-03 20:22:30 -0300

    suma pares seguido

--- sumapares.py
+++ sumapares.py
@@ -1,13 +1,28 @@
-import os, argparse
+import os 
+import argparse
+
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", type=int, help="Numero de procesos que se quieren generar", required=True)
     parser.add_argument("-v",help="-v para habiltiar modo verboso",action="stroe true",default=False)
-    args = parser.parse_args
+    args = parser.parse_args()
+    numero = args.numero
+    verboso = args.verboso
 
-def suma()
+
+
+
+pid = os.fork
+
+def suma(pid):
+    sumapares = 0
+    for n in range(pid):
+        if n%2 == 0:
+            sumapares += n
+    return sumapares
+
     
 
 
